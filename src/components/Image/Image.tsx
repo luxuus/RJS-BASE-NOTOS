d5191d1827b8050501c828ede424a2ea7110dd59
Tony NOEL <tnoel@armonie.group>
2025-10-17 16:53:11 +0200

    Complétion Composant Image

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -1,42 +1,54 @@
 /* Global Imports */
-import React, { FC } from 'react';
+import React, { FC } from "react";
 
 /* Application Level Imports */
-import * as Hooks from '@/hooks';
+import * as Hooks from "@/hooks";
 
 /* Local Imports */
-import { ImageWrapper } from './Image.styled';
+import { ImageWrapper } from "./Image.styled";
 
-
-interface ImageProps { }
-
-
-const Image: FC<ImageProps> = () => {
-
-   // Hooks.useGloblaEvent('click',()=> console.log('click event'));
-
-   return(
-   <ImageWrapper data-testid="Image">
-      Image Component
-   </ImageWrapper>
-   );
-
+interface ImageProps {
+  url: string;
+  description: string;
+  width?: number;
+  height?: number;
+  aspectRactio?: string;
 }
+
+const Image: FC<ImageProps> = ({
+  url,
+  description,
+  width,
+  height,
+  aspectRactio,
+}) => {
+  return (
+    <ImageWrapper data-testid="Image">
+      <img
+        src={url}
+        alt={description}
+        width={width}
+        height={height}
+        style={{ aspectRatio: aspectRactio }}
+      />
+    </ImageWrapper>
+  );
+};
 
 /**
  * USAGE: Image description to complete.
  * @example
- * <Image /> 
+ * <Image />
  */
 const ImageMemo = React.memo(Image, (prevProps, nextProps) => {
-   /*
+  /*
    Compare props to prevent unnecessary re-renders
    return true if props are equal
    return false if props are not equal
    */
-   console.log(prevProps, nextProps)
-   return true;
+  console.log(prevProps, nextProps);
+  return true;
 });
-ImageMemo.displayName = 'Image Memoized';
+ImageMemo.displayName = "Image Memoized";
 
 export default ImageMemo;
--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -40,6 +40,13 @@ const Home: FC<HomeProps> = () => {
         >
           Click Me
         </UI.Button>
+        <UI.Image
+          url={
+            "https://images.unsplash.com/photo-1548772981-2fded21624d9?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=2574"
+          }
+          description={"Mountain"}
+          width={500}
+        />
       </UI.Main>
       <UI.Footer>
         <UI.Footer.Content label={"Socials"}>
